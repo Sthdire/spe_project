2457de84867d41a4a7a0a869a7ecb34622c22ac9
Sthdire <demihidden@mail.ru>
2022-08-09 02:38:08 +0300

    ++

--- init.py
+++ init.py
@@ -2,5 +2,3 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from init import app
 def hello_world():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
