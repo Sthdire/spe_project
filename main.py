90884456a968661ec7dde348dca6ec33238d14e6
Settter <egor-udon@mail.ru>
2022-08-10 23:27:17 +0300

    comment block backend

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request
 
 from init import app
-from test import push_comm
+import test
 
 
 @app.route('/', methods=['post', 'get'])
 def hello_world():
     if request.method == 'POST':
-        push_comm(request.form.get('username'), request.form.get('comment'))
-    return render_template('index.html')
+        test.push_comm(request.form.get('username'), request.form.get('comment'))
+    i = 0
+    username = ['', '', '', '', '', '', '', '', '', '']
+    com_value = ['', '', '', '', '', '', '', '', '', '']
+    while i < 10:
+        username[i] = test.get_comm_username(i + 1)
+        com_value[i] = test.get_comm_value(i + 1)
+        i += 1
+    return render_template('index.html', username=username, com_value=com_value)  # , username = username, comment= comment
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 from pymongo import MongoClient
 
+client = MongoClient('localhost', 27017)
+db = client["py_app_db"]
+comments_collection = db["comments"]
+
+cursor = comments_collection.find({})
+count = comments_collection.count_documents({})
+
 
 def push_comm(username, comm):
-    client = MongoClient('localhost', 27017)
-    db = client["py_app_db"]
-    comments_collection = db["comments"]
-
     comment = {
         'username': username,
         'com_value': comm
     }
-
     comments_collection.insert_one(comment)
-    print(db.comments_collection.find())
+
+
+def get_comm_username(x):
+    username = cursor[count - x]['username']
+    return username
+
+
+def get_comm_value(x):
+    com_value = cursor[count - x]['com_value']
+    return com_value
