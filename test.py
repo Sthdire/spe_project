eb481f39b44009965c9b5a2032640cf8248c9f61
Settter <egor-udon@mail.ru>
2022-08-21 21:49:26 +0300

    remember

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client["py_app_db"]
 comments_collection = db["comments"]
-comments_collection.drop()
+
 def push_comm(username, comm):
     comment = {
         'username': username,
