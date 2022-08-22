d15878740411810c1ce993ef4a1d429df823b516
Settter <egor-udon@mail.ru>
2022-08-22 18:56:24 +0300

    db file refactoring

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, request
 
 from init import app
-import test
+import mongo_db
 
 
 @app.route('/', methods=['post', 'get'])
 def hello_world():
     if request.method == 'POST':
-        test.push_comm(request.form.get('username'), request.form.get('comment'))
+        mongo_db.push_comm(request.form.get('username'), request.form.get('comment'))
     i = 0
     username = ['', '', '', '', '', '', '', '', '', '']
     com_value = ['', '', '', '', '', '', '', '', '', '']
     while i < 10:
-        username[i] = test.get_comm_username(i + 1)
-        com_value[i] = test.get_comm_value(i + 1)
+        username[i] = mongo_db.get_comm_username(i + 1)
+        com_value[i] = mongo_db.get_comm_value(i + 1)
         i += 1
     return render_template('index.html', username=username, com_value=com_value)  # , username = username, comment= comment
 
